fb73fbf1de781d047a86a77e3877a69f97e98849
li-shan-asked <1477153217@qq.com>
2026-04-11 12:09:04 +0800

    update

--- src/components/SoftwareInfo.vue
+++ src/components/SoftwareInfo.vue
@@ -2,5 +2,9 @@
   <div class="page-container">
     <h2>软件说明</h2>
     <p>软件说明与使用介绍</p>
+    <p>
+      SGL地址：<br>
+      <a href="https://github.com/sgl-org/sgl" target="_blank" rel="noopener noreferrer">https://github.com/sgl-org/sgl</a>
+    </p>
   </div>
 </template>
